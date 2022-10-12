4e7413a03ae99ea3a94cfce3ba0a38c417b0fab1
Freddy Zwirb <fzwirb@gmail.com>
2022-10-11 20:50:20 -0600

    cleaned up play_button.js and added documentation

--- play_button.js
+++ play_button.js
@@ -1,4 +1,4 @@
-import React, { Component, Audio } from 'react'
+import React, { Component } from 'react'
 import Play from './play'
 import Pause from './pause'
 import { View } from 'react-native'
@@ -6,6 +6,9 @@ import { View } from 'react-native'
 import Sound from 'react-native-sound';
 Sound.setCategory('Playback');
 
+/**
+ * Init the variable to store the sound used by the metronome
+ */
 var click = new Sound('Synth_Block_E_lo.wav', Sound.MAIN_BUNDLE, (error) => {
   if (error) {
     console.log('failed to load the sound', error);
@@ -17,56 +20,29 @@ var click = new Sound('Synth_Block_E_lo.wav', Sound.MAIN_BUNDLE, (error) => {
 });
 click.setVolume(1);
 
-
 class Player extends Component {
 
 constructor(props) {
   super(props)
-  let playbool = false
   this.state = {
     playing: false
   }
-  this.startAndStop = this.startAndStop.bind(this) //bind the state to the play method so that it can be used
-
 }
-
-handlePlayerClick = () => {
-    console.log("in handle player click")
-    console.log(this.state.playing)
-
-  if (!this.state.playing) {
-    console.log("IN FIRST IF")
-    this.setState({playing: true}, 
-      () => {this.handlePlayMet()});
-
-  } else {
-    console.log("IN ELSE")
-    this.setState({playing: false}, 
-      () => {this.handlePlayMet()});  
-    console.log("DONE")
-    return
-  }
-} 
-
-returnFunct = () => {
-  console.log("RETURN")
-  return
-}
-updatePlayingInLoop = () => {
-    console.log("updatePLAYINGLOOP CALLED")
-    return inner = this.state.playing
-}
-
+/**
+ * Method that handles playing the audio of the metronome
+ */
 playClick = () => {
     click.play(success => {
         if (success) {
-          console.log('successfully finished playing');
+          // console.log('successfully finished playing');
         } else {
           console.log('playback failed due to audio decoding errors');
         }
       });  
 }
-
+/**
+ * Method called when the button is pressed. Handles starting and stopping the audio
+ */
 startAndStop = () =>{ 
   if(this.state.playing) {
     console.log("HERE")
@@ -79,17 +55,6 @@ startAndStop = () =>{
     this.setState({playing: true}, this.playClick);
   }
 }
-
-handlePlayMet = () => {
-    console.log("in play met!")
-    console.log(this.state.playing)
-    this.playbool = this.state.playing
-    let inner = this.playbool
-    console.log("PLAYBOOL: " + this.playbool)
-    this.startAndStop()
-}
-
-
   render() {
     return (
       <View className="player" >
